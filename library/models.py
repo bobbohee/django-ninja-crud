7e5a1f78a990ffa92023946add6308eb00c5d8eb
bobbohee <parkbohee0705@gmail.com>
2022-03-21 21:51:12 +0900

    [ADD] 책(Book) 및 출판사(Publisher) 모델 추가

--- library/models.py
+++ library/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Book(models.Model):
+    isbn = models.CharField(primary_key=True, max_length=13)
+    name = models.CharField(max_length=20)
+    sub_name = models.CharField(max_length=50)
+    publisher = models.ForeignKey(Publisher, on_delete=models.PROTECT)
+    authors = models.ManyToManyField(User)
+    price = models.IntegerField()
+    pages = models.IntegerField()
+    release_date = models.DateField()
+    create_date = models.DateTimeField(auto_now_add=True, editable=False)
+    update_date = models.DateTimeField(auto_now=True, editable=False)
