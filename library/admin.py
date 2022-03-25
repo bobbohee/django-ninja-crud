22a63041d50e3812f11e2eba3bc8fb3c2335bc5c
bobbohee <parkbohee0705@gmail.com>
2022-03-25 21:57:52 +0900

    [ADD] Admin에 출판사(Publisher), 작가(Author), 책(Book) 모델 추가

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from library.models import Author, Book, Publisher
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
